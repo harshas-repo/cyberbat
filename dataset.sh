da18191152403746f638b5d0fc160ef8dfdb79a2
Harshavardhan b <harsha00159@gmail.com>
2021-04-21 23:15:06 +0530

    Fixed dataset.sh

--- dataset.sh
+++ dataset.sh
@@ -1,18 +1,18 @@
-!curl -o dataset1.zip https://zenodo.org/record/3678171/files/dev_data_fan.zip?download=1
-!curl -o dataset2.zip https://zenodo.org/record/3678171/files/dev_data_pump.zip?download=1
-!curl -o dataset3.zip https://zenodo.org/record/3678171/files/dev_data_slider.zip?download=1
-!curl -o dataset4.zip https://zenodo.org/record/3678171/files/dev_data_ToyCar.zip?download=1
-!curl -o dataset5.zip https://zenodo.org/record/3678171/files/dev_data_ToyConveyor.zip?download=1
-!curl -o dataset6.zip https://zenodo.org/record/3678171/files/dev_data_valve.zip?download=1
-!unzip /content/dataset1.zip
-!rm -rf /conten/dataset1.zip
-!unzip /content/dataset2.zip
-!rm -rf /content/dataset2.zip
-!unzip /content/dataset3.zip
-!rm -rf /content/dataset3.zip
-!unzip /content/dataset4.zip
-!rm -rf /content/dataset4.zip
-!unzip /content/dataset5.zip
-!rm -rf /content/dataset5.zip
-!unzip /content/dataset6.zip
-!rm -rf /content/dataset6.zip
+curl -o dataset1.zip https://zenodo.org/record/3678171/files/dev_data_fan.zip?download=1
+curl -o dataset2.zip https://zenodo.org/record/3678171/files/dev_data_pump.zip?download=1
+curl -o dataset3.zip https://zenodo.org/record/3678171/files/dev_data_slider.zip?download=1
+curl -o dataset4.zip https://zenodo.org/record/3678171/files/dev_data_ToyCar.zip?download=1
+curl -o dataset5.zip https://zenodo.org/record/3678171/files/dev_data_ToyConveyor.zip?download=1
+curl -o dataset6.zip https://zenodo.org/record/3678171/files/dev_data_valve.zip?download=1
+unzip /content/dataset1.zip
+rm -rf /conten/dataset1.zip
+unzip /content/dataset2.zip
+rm -rf /content/dataset2.zip
+unzip /content/dataset3.zip
+rm -rf /content/dataset3.zip
+unzip /content/dataset4.zip
+rm -rf /content/dataset4.zip
+unzip /content/dataset5.zip
+rm -rf /content/dataset5.zip
+unzip /content/dataset6.zip
+rm -rf /content/dataset6.zip
